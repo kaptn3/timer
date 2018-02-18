9db2d8f3d61a5a294882f47b97411a047f98aa58
vikapitoshka <kapitonenko.vick@ya.ru>
2018-02-18 20:04:49 +0300

    fix bug with month

--- script.js
+++ script.js
@@ -3,24 +3,23 @@ setTimePage = (d,h,m,s) => {
   e2.innerHTML = h;
   e3.innerHTML = m;
   e4.innerHTML = s;
-  if ( (d < 0) && (h < 0) && (m < 0) && (s <= 0) ) {
-    e1.innerHTML = '0';
-    e2.innerHTML = '0';
-    e3.innerHTML = '0';
-    e4.innerHTML = '0';
-  }
 }
 const timer = () => {
-  const endDate = new Date(2017,11,01); // дата дня
+  const endDate = new Date(2018,03,18); // дата дня
   let currentDate = new Date();
-  let seconds = (endDate-currentDate)/1000;
+  endDate.setMonth(endDate.getMonth() - 1); // месяцы от 0 до 11
+  let days = 0;
+  let hours = 0;
+  let minutes = 0;
+  let sec = 0; 
 
-  let days = Math.floor(seconds/(24*60*60));
-  let rest = seconds%(24*60*60);
-  let hours = Math.floor(rest/(60*60));
-  let minutes = Math.floor((rest%3600)/60);
-  let sec =  Math.floor((rest%(3600))%60);
-  
+  let ms = endDate.getTime() - currentDate.getTime();
+  if (ms > 0) {
+    days = Math.floor(ms / (1000 * 60 * 60 * 24 ));
+    hours = 24 - currentDate.getHours() - 1;
+    minutes = 60 - currentDate.getMinutes() - 1;
+    sec = 60 - currentDate.getSeconds();
+  }
   setTimePage(days, hours, minutes, sec);
 }
 
