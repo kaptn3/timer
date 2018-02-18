105cd4485890818bb4b51bb1bc2579be59792d88
vikapitoshka <kapitonenko.vick@ya.ru>
2018-02-18 17:56:53 +0300

    add end timer

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 setTimePage = (d,h,m,s) => {
-  const e1 = document.querySelector(".days");
-  const e2 = document.querySelector(".hours");
-  const e3 = document.querySelector(".minutes");
-  const e4 = document.querySelector(".sec");
-
   e1.innerHTML = d;
   e2.innerHTML = h;
   e3.innerHTML = m;
   e4.innerHTML = s;
+  if ( (d < 0) && (h < 0) && (m < 0) && (s <= 0) ) {
+    e1.innerHTML = '0';
+    e2.innerHTML = '0';
+    e3.innerHTML = '0';
+    e4.innerHTML = '0';
+  }
 }
 const timer = () => {
-  const endDate = new Date(2017,11,01);
+  const endDate = new Date(2017,11,01); // дата дня
   let currentDate = new Date();
   let seconds = (endDate-currentDate)/1000;
 
@@ -19,16 +20,14 @@ const timer = () => {
   let hours = Math.floor(rest/(60*60));
   let minutes = Math.floor((rest%3600)/60);
   let sec =  Math.floor((rest%(3600))%60);
-  //console.log(days, hours, minutes, sec);
-  //let hours = 
-   setTimePage(days, hours, minutes, sec);
-    
-  timerId = setInterval(timer, 1000) // устанавливаем вызов функции через каждую секунду
-  //let minutes = seconds/60;
-  //let hours = minutes/60;
-  //let days = hours/24;
-}
   
-window.onload = () => {
-  timer();
+  setTimePage(days, hours, minutes, sec);
 }
+
+const e1 = document.querySelector(".days");
+const e2 = document.querySelector(".hours");
+const e3 = document.querySelector(".minutes");
+const e4 = document.querySelector(".sec");
+timer();
+// устанавливаем вызов функции через каждую секунду
+timerId = setInterval(timer, 1000);
